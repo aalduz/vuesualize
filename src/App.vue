e6132aa02e7d7200b36e1af518ef7faed96f0a63
aalduz <aldogeneral@gmail.com>
2017-12-05 00:42:20 +0100

    a bit of cleanup

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
       <h2>User journey</h2>
       <div class="row">
         <div class="col-12">
-          <template v-if="steps.length > 0">
             <div class="steps-container row justify-content-lg-center">
               <div v-for="(step, index) in steps" class="col-12 col-lg-10 ">
                 <div v-bind:class="[step.childs ? 'user-journey-step-container--with-childs': 'user-journey-step-container--no-childs', index == steps.length - 1 ? 'last-step' : '']"
@@ -26,7 +25,6 @@
                         </div>
                       </div>
                     </template>
-
                     <div class="card-body">
                       <template v-if="step.descriptionInViewMode">
                         <p v-on:click="step.descriptionInViewMode = !step.descriptionInViewMode" class="card-text">{{ step.description }}</p>
@@ -37,6 +35,9 @@
                       </template>
                     </div>
                     <img :src="step.imageSrc" class="card-img-bottom">
+                    <template v-if="(!step.nameInViewMode || !step.descriptionInViewMode)">
+                        <button @click="deleteStep(index)" class="btn btn-danger btn-sm">Delete step</button>
+                    </template>
                   </div>
                   <template v-if="step.childs">
                     <div class="row">
@@ -71,37 +72,44 @@
                 </div>
               </div>
             </div>
-          </template>
-          <template v-else>
             <div class="container user-journey--empty">
               <div class="row justify-content-center">
                 <div class="col-12">
-                  <template v-if="steps.length == 0 & !addStepStarted">
-                    <h3>It seems you dont have any steps defined on your user journey.</h3>
-                    <h4>Why dont you start creating the first one?</h4>
+                  <template v-if="!addStepStarted">
+                    <template v-if="steps.length == 0">
+                      <h3>It seems you dont have any steps defined on your user journey.</h3>
+                      <h4>Why dont you start creating the first one?</h4>
+                    </template>
+                    <template v-else>
+                      <h3 class="clarification">You can continue by adding an extra step</h3>
+                      <h4>Or by editing previous steps</h4>
+                      <!-- <h4>Or even adding childs to the last one</h4> -->
+                    </template>
                     <div class="button-container">
+                      <!-- <button v-on:click="addChildStarted = true;addStepStarted = true" v-if="steps.length > 0" class="btn btn-secondary">Add child</button> -->
                       <button v-on:click="addStepStarted = true" class="btn btn-primary">Add step</button>
                     </div>
                   </template>
                   <template v-else>
+                    <h3 v-if="steps.length > 0" class="clarification"></h3>
                     <div class="steps-container row justify-content-lg-center">
-                      <div class="col-10">
+                      <div class="col-12 col-10">
                         <div class="card user-journey-step">
                           <div class="container edit-mode-container" id="firstStep">
                             <div class="form-group">
-                              <label for="stepName">Step Name</label>
+                              <label for="stepName">Step Name *</label>
                               <input v-model="newStep.name" type="text" class="form-control" id="stepName" aria-describedby="stepName" placeholder="Enter the name of the step">
                               <small id="nameHelp" class="form-text text-muted">It can also be the name of the page where the step is happening</small>
                             </div>
                             <div class="form-group">
-                              <label for="stepDescription">Step description</label>
+                              <label for="stepDescription">Step description *</label>
                               <textarea v-model="newStep.description" type="text" class="form-control" rows="10" id="stepDescription"></textarea>
                               <small id="descriptionHelp" class="form-text text-muted">Describe with as much as detail as you think it might be necessary.</small>
                             </div>
                             <div class="row justify-content-end">
                               <div class="col-12">
                                 <button v-on:click="addStepStarted = false" class="btn btn-light">Cancel</button>
-                                <button v-on:click="createStep(newStep)" class="btn btn-primary">Save step</button>
+                                <button v-on:click="createStep(newStep, addChildStarted)" class="btn btn-primary">Save step</button>
                               </div>
                             </div>
                           </div>
@@ -112,7 +120,6 @@
                 </div>
               </div>
             </div>
-          </template>
         </div>
       </div>
     </div>
@@ -126,23 +133,53 @@ export default {
     src: (assetName) => {
       return require(assetName);
     },
-    createStep(newStep) {
-      console.log(this.newStep, newStep);
-      this.steps.push({
-        number: this.steps.length + 1,
+    isNewStepEmpty: (newStep) => {
+      return newStep.name != '' && newStep.description != '';
+    },
+    addNewStepToArray(newStep, array) {
+      array.push({
+        number: array.length + 1,
         name: newStep.name,
         nameInViewMode: true,
         description: newStep.description,
         descriptionInViewMode: true,
-        imageSrc: ''
+        imageSrc: '',
+        childs: false
       });
-      newStep.name = '';
-      newStep.description = '';
+    },
+    createStep(newStep, isChild) {
+      if(this.isNewStepEmpty) {
+        var stepsLength = this.steps.length;
+
+        if (isChild) {
+          var lastStep = this.steps[stepsLength - 1];
+
+          lastStep.childs == false ? lastStep.childs = [] : '';
+          this.addNewStepToArray(newStep, this.steps[stepsLength - 1].childs);
+          this.addChildStarted = false;
+        } else {
+          this.addNewStepToArray(newStep, this.steps)
+          this.addStepStarted = false;
+        }
+        newStep.name = '';
+        newStep.description = '';
+      } else {
+        alert('Please fill in the fields');
+      }
+    },
+    deleteStep(index) {
+      var stepToDelete = this.steps[index];
+      this.steps = this.steps.filter(step => step !== stepToDelete);
+
+      this.steps.forEach((step,index) => {
+        step.number = index + 1;
+      });
     }
   },
   data () {
     return {
       addStepStarted: false,
+      addChildStarted: false,
       newStep: {
         name: '',
         description: '',
@@ -238,11 +275,20 @@ export default {
   text-align: center;
 }
 
+.user-journey--empty h3.clarification {
+  border-top: 1px solid white;
+  padding-top: 3rem;
+}
+
 .user-journey--empty .button-container {
     display: flex;
     flex-direction: row;
     justify-content: center;
     margin: 2rem;
+
+    button {
+      margin: 0 1rem;
+    }
 }
 
 @import "styles/index";
