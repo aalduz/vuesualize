873c6fe8ab51f00916a53dfe630ef4530edc4817
aalduz <aldogeneral@gmail.com>
2017-12-08 11:00:18 +0100

    VUESUAL-2: include thumbnail component

---
+++ src/Components/Thumbnail/Thumbnail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-on:click="goToElement" class="card card__overview-item">
+        <div class="card-img-top">
+            <img :src="imageSrc">
+        </div>
+        <div class="card-block">
+            <h4 class="class-title">{{ name }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'thumbnail',
+        methods: {
+            goToElement: function (element) {
+
+            },
+        },
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            imageSrc: {
+                type: String,
+                default: '../../assets/logo.png',
+            }
+        }
+    }
+</script>
+
+
