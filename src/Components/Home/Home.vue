778ba2408349062ebe55187ebdd45da3056a6e71
aalduz <aldogeneral@gmail.com>
2017-12-12 17:12:39 +0100

    VUESUAL-2: some needed components

---
+++ src/Components/Home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col-12 page-heading">
+            <h1 class="justify-content-lg-center">Home Page</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
---
+++ src/Components/Journey/JourneyDetail.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col-12 page-heading">
+            <h1 class="justify-content-lg-center">{{ journey.name }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { 
+        mapGetters,
+        mapMutations
+    } from 'vuex';
+    
+    export default {
+        computed: {
+            ...mapGetters([
+                'journey'
+            ])
+        },
+        methods: {
+            ...mapMutations([
+                'journey'
+            ])
+        },
+        beforeRouteLeave(to, from, next) {
+            this.$store.commit('journey', {});
+            next();
+        }
+    }
+</script>
