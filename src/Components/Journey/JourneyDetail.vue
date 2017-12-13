e7f239eedd075b3c9e0d05bbd2d868fc8b9a8671
aalduz <aldogeneral@gmail.com>
2017-12-13 10:35:40 +0100

    VUESAL-2: add existing code for Journey Detail component
    It needs to break it down to small components on Journey step new, view
    and edit

--- src/Components/Journey/JourneyDetail.vue
+++ src/Components/Journey/JourneyDetail.vue
@@ -3,6 +3,68 @@
         <div class="col-12 page-heading">
             <h1 class="justify-content-lg-center">{{ journey.name }}</h1>
         </div>
+        <div class="col-12">
+            <div class="steps-container row justify-content-lg-center">
+                <div v-for="(step, index) in steps" class="col-12 col-lg-10 ">
+                    <div class="user-journey-step-container">
+                        <div class="card user-journey-step">
+                      <template v-if="step.nameInViewMode">
+                        <h4 v-on:click="step.nameInViewMode = !step.nameInViewMode"  class="card-header"><span class="step-number">{{ step.number }}</span>{{ step.name }}</h4>
+                      </template>
+                      <template v-else>
+                        <div class="card-header edit-mode-container">
+                          <div class="row justify-content-center form-inline">
+                            <div class="col-10">
+                              <input type="text" v-model="step.name" class="form-control full">
+                            </div>
+                            <div class="col-2">
+                              <button v-on:click="updateJourney(step, 'name')" class="btn btn-primary  btn-block">Save</button>
+                            </div>
+                          </div>
+                        </div>
+                      </template>
+                      <div class="card-body">
+                        <template v-if="step.descriptionInViewMode">
+                          <p v-on:click="step.descriptionInViewMode = !step.descriptionInViewMode" class="card-text">{{ step.description }}</p>
+                        </template>
+                        <template v-else>
+                          <textarea type="text" v-model="step.description" class="form-control"></textarea>
+                          <br>
+                          <button v-on:click="updateJourney(step, 'description')" class="btn btn-primary">Save</button>
+                        </template>
+                      </div>
+                      <template v-if="step.imageInViewMode">
+                        <div class="card-block">
+                          <img v-if="step.imageSrc != ''" v-on:click="step.imageInViewMode = !step.imageInViewMode" :src="step.imageSrc" class="card-img-bottom">
+                          <p v-else v-on:click="step.imageInViewMode = !step.imageInViewMode" class="image-src"><i>Click here to provide an image</i></p>
+                        </div>
+                      </template>
+                      <template v-else>
+                        <div class="edit-mode-container">
+                          <div v-if="!fileUploading" class="row form-inline">
+                            <div class="col-10">
+                              <input type="text" v-model="step.imageSrc" class="form-control full">
+                              <input @change="uploadFile($event, step)" type="file">
+                            </div>
+                            <div class="col-2">
+                              <button v-on:click="updateJourney(step, 'image')" class="btn btn-primary btn-block">Save</button>
+                            </div>
+                          </div>
+                          <div v-else class="row form-inline">
+                            <div class="col-12">
+                              <loader></loader>
+                            </div>
+                          </div>
+                        </div>
+                      </template>
+                      <template v-if="(!step.nameInViewMode || !step.descriptionInViewMode || !step.imageInViewMode)">
+                          <button @click="deleteStep(index)" class="btn btn-danger btn-sm">Delete step</button>
+                      </template>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,12 +78,49 @@
         computed: {
             ...mapGetters([
                 'journey'
-            ])
+            ]),
+            steps: function() {
+                let stepsWithControlProperties = this.journey.steps;
+
+                    stepsWithControlProperties.forEach(step => {
+                        step.nameInViewMode = true;
+                        step.descriptionInViewMode = true;
+                        step.imageInViewMode = true;
+                        step.tagsInViewMode = true;
+                    });
+               
+                return stepsWithControlProperties;
+            }
+        },
+        data () {
+            return {
+            }
         },
         methods: {
             ...mapMutations([
                 'journey'
-            ])
+            ]),
+            updateJourney: function(step, field) {
+                let journey = this.journey;
+                let journeyKey = journey['.key'];
+
+                // create a copy of the journey
+                journey = {...journey};
+                // remove the .key attribute
+                delete journey['.key'];
+
+                // update the object in firebase
+                this.$firebaseRefs.journeys.child(journeyKey).set(journey);
+
+                // Set back to viewMode the field that was just edited
+                if (field === 'name') {
+                    step.nameInViewMode = true;
+                } else if (field === 'description') {
+                    step.descriptionInViewMode = true;
+                } else {
+                    step.imageInViewMode = true;
+                }
+            },
         },
         beforeRouteLeave(to, from, next) {
             this.$store.commit('journey', {});
