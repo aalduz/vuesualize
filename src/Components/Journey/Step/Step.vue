44e6e1f0560a6807e14940efb8f4f4cda77f64f2
aalduz <aldogeneral@gmail.com>
2017-12-21 17:43:39 +0100

    VUESUAL-5: add Step component to use it within JourneyDetail
    Also change image arrangement in card

---
+++ src/Components/Journey/Step/Step.vue
@@ -0,0 +1,59 @@
+<template>
+    <transition
+        enter-active-class="animated rubberBand">
+        <div class="user-journey-step-container"
+            :class="userJourneyStepContainerClasses">
+            <template v-if="!isEditMode">
+                <div class="card user-journey-step">
+                    <div class="card-header">
+                        <h4>
+                            <span class="step-number">{{ index + 1 }}</span>
+                            {{step.name}}
+                        </h4>
+                        <template v-if="step.tags">
+                            <div class="tags-container">
+                                <span v-for="(tag,index) in step.tags"
+                                    v-if="index < 3"
+                                    class="badge badge-pill badge-warning">{{ tag }}</span>
+                                <span v-if="step.tags.length > 3">...</span>
+                            </div>
+                        </template>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">{{step.description}}</p>
+                    </div>
+                    <img 
+                        v-if="step.imageSrc"
+                        :src="step.imageSrc"
+                        class="card-img-bottom">
+                </div>
+            </template>
+            <template v-else>
+
+            </template>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: ['step', 'index'],
+    computed: {
+        userJourneyStepContainerClasses () {
+            return {
+                no_children : !this.step.childs,
+                with_children : this.step.childs
+            }
+        }
+    },
+    data () {
+        return {
+            isEditMode : false,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
